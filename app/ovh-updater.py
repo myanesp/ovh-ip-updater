8efe01cdc973a148be6b87c9018d4fb369a8ef7a
myanesp <17320863+myanesp@users.noreply.github.com>
2024-05-09 00:09:50 +0200

    fix: reorder files

---
+++ app/ovh-updater.py
@@ -0,0 +1,52 @@
+import ovh
+import requests
+import time
+import json
+import os
+import sys
+from config import *
+
+zone_name = os.environ.get("DOMAIN")
+sub = os.environ.get("SUBDOMAIN")
+ttl = os.environ.get("TTL")
+sec = os.environ.get("INTERVAL")
+ip_provider = os.environ.get("PROVIDER")
+
+ip = obtain_ip(ip_provider)
+
+if not ip:
+    print("Cannot obtain your public IP address. Please, check if there is something wrong with your firewall or connection and try again")
+    sys.exit()
+else:
+    print(f"IP obtained succesfully. Your current public IP is {ip}, using {ip_provider}")
+
+client = ovh.Client() 
+
+time.sleep(2)
+
+# Get the ID of the subdomain, needed for update its IP
+record_id = client.get(f'/domain/zone/{zone_name}/record', 
+    fieldType='A', 
+    subDomain=sub,
+)
+
+## If it is correct, check if the record is empty to know if it exists previously or not
+if record_id:
+
+    rec_id = record_id[0]
+
+    result = client.put(f'/domain/zone/{zone_name}/record/{rec_id}', 
+        subDomain=sub, 
+        target=ip, 
+        ttl=ttl, 
+    )
+
+    time.sleep(2)
+
+    refresh = client.post(f'/domain/zone/{zone_name}/refresh')
+    print(f"IP updated for {sub}.{zone_name} with IP {ip}.")
+
+elif not record_id:
+    print(f'The subdomain you have provided does not exist. Please, create it first on your OVH web console and try again.')
+    sys.exit()
+
