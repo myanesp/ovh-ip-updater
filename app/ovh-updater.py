7c4225b4d76a9e4cb03ca80af55be0613ae39392
myanesp <17320863+myanesp@users.noreply.github.com>
2024-10-17 16:13:07 +0200

    feat: bump version to 0.3.1

--- app/ovh-updater.py
+++ app/ovh-updater.py
@@ -5,9 +5,9 @@ import json
 import os
 import sys
 from config import *
-from datetime import datetime 
+from datetime import datetime
 
-version = "0.3"
+version = "0.3.1"
 zone_name = os.environ.get("DOMAIN")
 sub = os.environ.get("SUBDOMAIN")
 ttl = os.environ.get("TTL")
