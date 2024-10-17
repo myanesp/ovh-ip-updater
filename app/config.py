89cfc37cb54d1becae8737d4e07c7caa973a4199
myanesp <17320863+myanesp@users.noreply.github.com>
2024-10-17 15:24:41 +0200

    feat: Telegram notification when IP changes

--- app/config.py
+++ app/config.py
@@ -53,6 +53,6 @@ def count_sub(env):
 
 def send_message(ip, domain, chat, token):
 
-    message = f"{tims()} - OVH IP UPDATER\n\nChange in your IP, now is {ip}.\nThe subdomains of {domain} have been updated with this new IP."
+    message = f"{tims()} - OVH IP UPDATER\n\nYour IP has changed, now is {ip}.\nThe subdomains of {domain} have been updated with this new IP."
     url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat}&text={message}"
     requests.get(url)
