35afaeb233faedde2861646e1061831280992a26
myanesp <17320863+myanesp@users.noreply.github.com>
2024-11-18 18:13:35 +0100

    feat: create subdomain when not found

--- app/ovh-updater.py
+++ app/ovh-updater.py
@@ -7,7 +7,7 @@ import sys
 from config import *
 from datetime import datetime
 
-version = "0.3.2"
+version = "0.4"
 zone_name = os.environ.get("DOMAIN")
 sub = os.environ.get("SUBDOMAIN")
 ttl = os.environ.get("TTL")
@@ -68,8 +68,21 @@ if n_sub == 1:
                 send_message(ip = ip, domain = zone_name, chat = tg_chat, token = tg_token)
 
     elif not record_id:
-        print(f'{tims()} The subdomain you have provided does not exist. Please, create it first on your OVH web console and try again.')
-        sys.exit()
+        print(f'{tims()} The subdomain you have provided does not exist. Trying to create it...')
+
+        create = client.post(f"/domain/zone/{zone_name}/record",
+            target = ip,
+            ttl = 60, 
+            fieldType = "A", 
+            subDomain = sub, 
+        )
+
+        refresh = client.post(f'/domain/zone/{zone_name}/refresh')
+        print(f"{tims()} {sub}.{zone_name} created and assigned IP {ip}.")
+        if tg_token == None:
+            pass
+        else:
+            send_create(ip = ip, sub = sub, domain = zone_name, chat = tg_chat, token = tg_token)
 
 elif n_sub > 1:
     sub_list = sub.split(',')
@@ -102,8 +115,21 @@ elif n_sub > 1:
                 print(f"{tims()} IP updated for {sub}.{zone_name} with IP {ip}.")
 
         elif not record_id:
-            print(f'{tims()} The subdomain you have provided does not exist. Please, create it first on your OVH web console and try again.')
-            sys.exit()
+            print(f'{tims()} The subdomain you have provided does not exist. Trying to create it...')
+
+            create = client.post(f"/domain/zone/{zone_name}/record",
+                target = ip,
+                ttl = 60, 
+                fieldType = "A", 
+                subDomain = sub, 
+            )
+
+            refresh = client.post(f'/domain/zone/{zone_name}/refresh')
+            print(f"{tims()} {sub}.{zone_name} created and assigned IP {ip}.")
+            if tg_token == None:
+                pass
+            else:
+                send_create(ip = ip, sub = sub, domain = zone_name, chat = tg_chat, token = tg_token)
 
         refresh = client.post(f'/domain/zone/{zone_name}/refresh')
         if tg_token == None:
